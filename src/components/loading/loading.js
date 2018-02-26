TasvirChi/playchi-js-ui

commit 1c6d063276bde326c1c3348f8a917c9618a42a4a

fix(FEC-7368): no spinner between ads and spinner doesn't stop on autoplay on native adapter (#156)

src/components/loading/loading.js
@@ -37,6 +37,7 @@ class Loading extends BaseComponent {
    */
   constructor(obj: Object) {
     super({name: 'Loading', player: obj.player});
+    this.setState({afterPlayingEvent: false});
     try {
       // TODO: Change the dependency on ima to our ads plugin when it will be developed.
       if (this.player.config.playback.preload === "auto" && !this.player.config.plugins.ima) {
@@ -89,6 +90,9 @@ class Loading extends BaseComponent {
     }
 
     this.player.addEventListener(this.player.Event.PLAYER_STATE_CHANGED, e => {
+      if (!this.state.afterPlayingEvent) {
+        return;
+      }
       if (e.payload.newState.type === 'idle' || e.payload.newState.type === 'playing' || e.payload.newState.type === 'paused') {
         this.props.updateLoadingSpinnerState(false);
       }
@@ -111,6 +115,15 @@ class Loading extends BaseComponent {
 
     this.player.addEventListener(this.player.Event.ALL_ADS_COMPLETED, () => {
       this.props.updateLoadingSpinnerState(false);
+    });
+
+    this.player.addEventListener(this.player.Event.PLAYING, () => {
+      this.setState({afterPlayingEvent: true});
+      this.props.updateLoadingSpinnerState(false);
+    });
+
+    this.player.addEventListener(this.player.Event.CHANGE_SOURCE_STARTED, () => {
+      this.setState({afterPlayingEvent: false});
     });
   }
 

src/components/loading/_loading.scss
@@ -32,6 +32,7 @@
   background-color: rgba(0, 0, 0, 0.3);
   transition: 100ms opacity;
   opacity: 0;
+  z-index: 5;
 
   &.show {
     opacity: 1;

commit 33f486473d708cdb799aa3bf3278b07401be8328

fix(FEC-7956): When autoplay fails, loading spinner stays in the background (#183)
Hide loading spinner when autoplay failed event is triggered.
Also, get rid of the mobileAutoplay flags since its redundant and doesn't exists in the player.

caused by #156

src/components/loading/loading.js
@@ -27,7 +27,6 @@ const mapStateToProps = state => ({
    */
 class Loading extends BaseComponent {
   autoplay: boolean;
-  mobileAutoplay: boolean;
 
   /**
    * Creates an instance of Loading.
@@ -48,17 +47,10 @@ class Loading extends BaseComponent {
   componentWillMount() {
     try {
       this.autoplay = this.player.config.playback.autoplay;
-    }
-    catch (e) { // eslint-disable-line no-unused-vars
+    } catch (e) { // eslint-disable-line no-unused-vars
       this.autoplay = false;
     }
 
-    try {
-      this.mobileAutoplay = this.player.config.playback.mobileAutoplay;
-    }
-    catch (e) { // eslint-disable-line no-unused-vars
-      this.mobileAutoplay = false;
-    }
   }
 
   /**
@@ -70,18 +62,20 @@ class Loading extends BaseComponent {
    * @memberof Loading
    */
   componentDidMount() {
-    if (!this.props.isMobile && this.autoplay || this.props.isMobile && this.mobileAutoplay) {
+    if (this.autoplay) {
       this.props.updateLoadingSpinnerState(true);
     }
 
     this.player.addEventListener(this.player.Event.PLAYER_STATE_CHANGED, e => {
+      const StateType = this.player.State;
       if (!this.state.afterPlayingEvent) {
         return;
       }
-      if (e.payload.newState.type === 'idle' || e.payload.newState.type === 'playing' || e.payload.newState.type === 'paused') {
+      if (e.payload.newState.type === StateType.IDLE
+        || e.payload.newState.type === StateType.PLAYING
+        || e.payload.newState.type === StateType.PAUSED) {
         this.props.updateLoadingSpinnerState(false);
-      }
-      else {
+      } else {
         this.props.updateLoadingSpinnerState(true);
       }
     });
@@ -99,6 +93,10 @@ class Loading extends BaseComponent {
     });
 
     this.player.addEventListener(this.player.Event.ALL_ADS_COMPLETED, () => {
+      this.props.updateLoadingSpinnerState(false);
+    });
+
+    this.player.addEventListener(this.player.Event.AUTOPLAY_FAILED, () => {
       this.props.updateLoadingSpinnerState(false);
     });
 